fcd237e26ceea27773bdce34589ae375571d2f80
sqy <812919879@qq.com>
2020-05-27 00:15:17 +0800

    aaa

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,49 @@
 		margin: 0;
 		padding: 0;
 	}
+	.el-form-item label{
+		font-size: 26px !important;
+	}
+	.el-input{
+		font-size: 26px !important;
+		input{
+			height: 60px !important;
+			line-height: 60px !important;
+		}
+	}
+	.el-select-dropdown__item{
+		height: 60px !important;
+		span{
+			font-size: 26px !important;
+		}
+		line-height: 60px !important;
+		
+	}
+	.el-scrollbar{
+		height: auto !important;
+	}
+	.el-input-number{
+		.el-input-number__increase,.el-input-number__decrease{
+			width: 60px;
+			height: 60px;
+			line-height: 60px;
+			font-size: 26px;
+		}
+	}
+	.el-form .el-form-item{
+		margin-bottom:30px;
+		.el-button{
+			font-size: 26px;
+			line-height: 60px;
+			width: 350px;
+		}
+		.el-select .el-input .el-select__caret{
+			font-size: 26px;
+			line-height: 60px;
+		}
+		.el-form-item__error{
+			font-size: 20px;
+		}
+	}
+	
 </style>
--- src/views/form.vue
+++ src/views/form.vue
@@ -1,40 +1,116 @@
 <template>
-	<el-form ref="form" :model="form">
-		<el-form-item label="活动名称">
-			<el-input v-model="form.name"></el-input>
-		</el-form-item>
-		<el-form-item label="活动区域">
-			<el-select v-model="form.region" placeholder="请选择活动区域">
-			<el-option label="区域一" value="shanghai"></el-option>
-			<el-option label="区域二" value="beijing"></el-option>
-			</el-select>
-		</el-form-item>
-		<el-form-item label="活动时间">
-			<el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
-		</el-form-item>
-		<el-form-item>
-			<el-button type="primary" @click="onSubmit">确认提交</el-button>
-		</el-form-item>
-	</el-form>
+	<div>
+		<header>
+			<gohome :width="60" :height="60"></gohome>
+		</header>
+		<el-form ref="form" id="form" label-width="150px" :label-position="labelPosition" :model="form" :rules="rules">
+			<el-form-item prop="name" label="姓名">
+				<el-input id="name" v-model="form.name" placeholder="请输入姓名"></el-input>
+			</el-form-item>
+			<el-form-item prop="number" label="手机号">
+				<el-input id="number" v-model.number="form.number" placeholder="请输入手机号"></el-input>
+			</el-form-item>
+			<el-form-item prop="cardNumber" label="身份证号">
+				<el-input id="cardNumber" v-model.number="form.cardNumber" placeholder="请输入身份证号"></el-input>
+			</el-form-item>
+			<el-form-item prop="region" label="选择房型">
+				<el-select style="width:100%" v-model="form.region" placeholder="请选择房型">
+					<el-option label="房型一" value="shanghai"></el-option>
+					<el-option label="房型二" value="beijing"></el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item label="入住天数">
+				<el-input-number v-model="form.num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+			</el-form-item>
+			<el-form-item label="价格">
+				<el-input v-model.number="form.jg" readonly></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="onSubmit">确认提交</el-button>
+			</el-form-item>
+		</el-form>
+  </div>
 </template>
 <script>
+import gohome from '../component/gohome'
 export default {
-    name: 'form',
-    data(){
-        return{
-            
-        }
-	},
-	methods:{
-		handleChange(value){
-			console.log(value);
-		},
-		onSubmit(){
-
+  name: "form",
+  data() {
+		var checkNumber = (rule, value, callback) => {
+		if (!value) {
+			return callback(new Error("手机号不能为空"));
 		}
-	}
-}
+		var reg = /^1[3456789]\d{9}$/;
+		if (!reg.test(value)) {
+			return callback(new Error("请输入正确的手机号"));
+		}
+		callback();
+	};
+	var checkCardNumber = (rule, value, callback) => {
+		if (!value) {
+			return callback(new Error("身份证号不能为空"));
+		}
+		var reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+		if (!reg.test(value)) {
+			return callback(new Error("请输入正确的身份证号"));
+		}
+		callback();
+	};
+    return {
+		labelPosition:"left",
+		form: {
+			name: "",
+			tel: "",
+			card: "",
+			region: "",
+			num: "",
+			jg: ""
+		},
+	  rules:{
+		name:[
+			{required:true,message:'请输入姓名',trigger:'blur'},
+			{min:2,message:'最少两个字符',trigger: 'blur'}
+		],
+		number:[
+			{required:true,validator:checkNumber,trigger:'blur'},
+		],
+		cardNumber:[
+			{required:true,validator:checkCardNumber,trigger:'blur'}
+		],
+		region:[
+			{required:true,message:'请选择房型',trigger:'change'}
+		]
+	  }
+    };
+  },
+  mounted(){
+	  var str="name";
+	  console.log($("#"+str+">input"))
+  },
+  methods: {
+    handleChange(value) {
+      console.log(value);
+    },
+    onSubmit() {
+		this.$refs["form"].validate((valid)=>{
+			if(valid){
+
+			}
+		})
+	},
+  },
+  components:{gohome}
+};
 </script>
 <style lang="scss" scoped>
-
+.el-form{
+	margin: 20px 50px 0;
+}
+header{
+	margin-left: 50px;
+	margin-top: 20px;
+	// position: fixed;
+	// left: 50px;
+	// top: 20px;
+}
 </style>
